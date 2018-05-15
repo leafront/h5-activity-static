99f0e0df66e76fe42a79c371fac37d49f2b912fc
leafront <yeliang@laiyifen.com>
2018-05-15 16:26:55 +0800

    modified 配置

--- config/index.js
+++ config/index.js
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'activity-static',
-    assetsPublicPath: 'http://10.0.0.27:8080/',
+    assetsPublicPath: 'http://10.0.0.27:8090/',
 
     /**
      * Source Maps
