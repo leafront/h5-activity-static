aba37b5b5f690b6be0f3e74d34d3c7a7d58558ac
leafront <yeliang@laiyifen.com>
2018-06-22 11:37:15 +0800

    update config

--- config/index.js
+++ config/index.js
@@ -85,6 +85,36 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   },
+  test: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../target/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../target'),
+    assetsSubDirectory: 'activity-static',
+    assetsPublicPath: 'http://m.test.laiyifen.com/',
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
 
   release: {
     // Template for index.html
@@ -93,7 +123,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../target'),
     assetsSubDirectory: 'activity-static',
-    assetsPublicPath: 'http://m.static.stg.laiyifen.com/',
+    assetsPublicPath: 'http://m.static.release.laiyifen.com/',
 
     /**
      * Source Maps
