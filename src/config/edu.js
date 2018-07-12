57cd2f6cab310f3462d0cc7c6a1ece54aa8d99d0
leafront <yeliang@laiyifen.com>
2018-07-12 16:22:34 +0800

    update config

--- src/config/edu.js
+++ src/config/edu.js
@@ -1,6 +1,6 @@
 
 const config = JSON.stringify({
-  "hostPath": "http://m.lyf.edu.laiyifen.com",
+  "hostPath": "",
   "companyId": 30,
   "platformId": 3,  //H5
   "staticPath": "http://static.lyf.edu.laiyifen.com",
