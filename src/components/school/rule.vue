2df9ea5af65cae7ec971d8fbbe23e56bc2e85c03
leafront <yeliang@laiyifen.com>
2018-09-14 16:17:47 +0800

    update rule.vue

--- src/components/school/rule.vue
+++ src/components/school/rule.vue
@@ -112,7 +112,7 @@
     height: 10.34rem;
     overflow-scrolling: touch;
     -webkit-overflow-scrolling: touch;
-    background: url(./images/school_rule_bg.jpg) #fff no-repeat;
+    background: url(./images/school_rule_bg.jpg) no-repeat;
     background-size: 100% auto;
     z-index: -10;
     padding: 0 .3rem;
