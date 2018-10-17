22e3f426bc9dd6705f9685fcccf90178f1c5eca7
YELIANG <yeliang@laiyifen.com>
2018-10-17 17:35:01 +0800

    update koi.js

--- src/model/koi.js
+++ src/model/koi.js
@@ -1,12 +1,12 @@
 
 import request from '@/widget/request'
 
-export const getKoiCouponNum = (data) => request('/api/promotion/dragonball/coupons', data)
+export const getKoiCouponNum = (data) => request('/activity-static/dragonball/coupons.json', data)
 
-export const submitCouponExchange = (data) => request('/api/promotion/dragonball/getCoupon',data)
+export const submitCouponExchange = (data) => request('/activity-static/dragonball/getCoupon.json',data)
 
-export const getCouponExchange = (data) => request('/api/promotion/dragonball/getCoupon',data)
+export const getCouponExchange = (data) => request('/activity-static/dragonball/getCoupon.json',data)
 
-export const getKoiInfo = (data) => request('/api/promotion/dragonball/main',data)
+export const getKoiInfo = (data) => request('/activity-static/dragonball/main.json',data)
 
 export const getSystemTime = (data) => request('/api/realTime/getTimestamp',data)
