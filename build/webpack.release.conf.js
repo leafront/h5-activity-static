330c1cbc028e640c2970c31cfe6064da5c85a449
YELIANG <yeliang@laiyifen.com>
2018-10-18 12:32:42 +0800

    update config

--- build/webpack.release.conf.js
+++ build/webpack.release.conf.js
@@ -88,14 +88,26 @@ const webpackConfig = merge(baseWebpackConfig, {
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: function (module) {
+      minChunks (module) {
         // any required modules inside node_modules are extracted to vendor
+
+        const pathName = path.join(__dirname, '../node_modules')
+        const baseName = path.join(__dirname, '../src')
+        const moduleList = [
+          pathName + 'vue',
+          pathName + 'vuex',
+          pathName + 'vue-router',
+          baseName + '/widget/store.js',
+          baseName + '/widget/ajax.js',
+          baseName + '/widget/request.js',
+          baseName + '/widget/utils.js',
+          baseName +'/widget/common.js',
+          baseName +'/widget/app.js'
+        ]
         return (
           module.resource &&
           /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
+          moduleList.indexOf(module.resource) > -1
         )
       }
     }),
@@ -103,7 +115,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
-      minChunks: Infinity
+      minChunks: 10
     }),
     // This instance extracts shared chunks from code splitted chunks and bundles them
     // in a separate chunk, similar to the vendor chunk
@@ -112,7 +124,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       name: 'app',
       async: 'vendor-async',
       children: true,
-      minChunks: 3
+      minChunks: 10
     }),
 
     // copy custom static assets
