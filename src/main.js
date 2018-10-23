d91deb41c549fa8fdd99d63a6ae133ecd5295d35
YELIANG <yeliang@laiyifen.com>
2018-10-23 16:43:17 +0800

    udpate main.js

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ router.beforeEach((to, from, next) => {
   document.title = to.meta.title
   const bgColor = to.meta.bgColor
   if (bgColor) {
-    document.body.backgroundColor = bgColor
+    document.body.style.backgroundColor = bgColor
   } else {
     document.body.style.backgroundColor = '#f5f5f5'
   }
