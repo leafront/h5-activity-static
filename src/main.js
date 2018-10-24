66717fded0da1f52164b4de4baffad08f077f03e
YELIANG <yeliang@laiyifen.com>
2018-10-24 14:39:59 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@
 import Vue from 'vue'
 import Application from './App'
 import app from '@/widget/app'
-import {
-  sendTracker
-} from '@/widget/tracker'
 import router from './router'
 import store from './store'
 import utils from '@/widget/utils'
