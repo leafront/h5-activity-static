66f52ff9ad6618453485e0aad3833a994fbb8a0a
关超 <guanchao@guanchaos-MacBook-Pro.local>
2018-10-26 16:43:08 +0800

    请求冲突

--- src/model/koi.js
+++ src/model/koi.js
@@ -1,8 +1,6 @@
 
 import request from '@/widget/request'
 
-export const getKoiIndex = (data) => request('/api/my/user/info', data)
-
 export const getKoiMerge = (data) => request('/api/promotion/dragonball/unionInfo', data)
 
 export const union = (data) => request('/api/promotion/dragonball/union', data)
